9f8d05a3982e8e8604b35478c68fbb9a500e336f
Arthur_Bai <18610940312@126.com>
2018-05-10 14:08:25 +0800

    good practice

--- web.py
+++ web.py
@@ -2,9 +2,11 @@ import bs4
 import requests
 from selenium import webdriver
 
-res = requests.get('http://baidu.com')
+res = requests.get('http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=ibm')
 res.encoding = 'UTF-8'
 
-bs = bs4.BeautifulSoup(res.text)
+bs = bs4.BeautifulSoup(res.text,'html.parser')
 
 elems = bs.select('.t a')
+
+elems[0]['href']
